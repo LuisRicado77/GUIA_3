635e46b9902a4be5ce7255d9d65662a460609b33
LuisRicardo77 <luissayas2018@gmail.com>
2024-04-02 19:36:37 -0500

    first commit

---
+++ InitializrSpringbootProject/src/main/java/com/ejemploenNetBeans/demo/ControladorInicio.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ejemploenNetBeans.demo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author ldric
+ */
+
+@RestController
+@Slf4j
+public class ControladorInicio {
+    @GetMapping("/")
+    public String inicio(){
+        log.info("Ejecutando el controlador de inicio");
+        return "este es el inicio v2";
+    }
+}
